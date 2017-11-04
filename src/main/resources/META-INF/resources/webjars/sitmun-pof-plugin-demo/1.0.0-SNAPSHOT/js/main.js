ac20bdf659c82109c70502ac74ccc1d562bf19e7
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2017-11-04 17:42:02 +0100

    Versión testeable del módulo de JS

--- src/main/resources/META-INF/resources/webjars/sitmun-pof-plugin-demo/1.0.0-SNAPSHOT/js/main.js
+++ src/main/resources/META-INF/resources/webjars/sitmun-pof-plugin-demo/1.0.0-SNAPSHOT/js/main.js
@@ -1,16 +1,38 @@
-var demo1 = $(
-    "<div id=\"change_div\" class=\"alert alert-info\">" +
-    "<strong id=\"replace_me\">Touch me!</strong>" +
-    "</div>");
+var DemoWidget = (function ($) {
+    "use strict";
 
-$(document).ready(
-    $('#toolbar').append(demo1)
-);
-$('#change_div')
-    .on("click", function () {
-        $.get("/api/demo/hello?name=you", function (data, status) {
-            $('#replace_me').text(data.message);
-            $('#change_div').removeClass("alert-info")
-                .addClass("alert-success");
-        });
-    });
+    return {
+
+        demo: function(pos) {
+
+            var demo1 = $(
+                "<div id=\"change_div\" class=\"alert alert-info\">" +
+                "<strong id=\"replace_me\">Touch me!</strong>" +
+                "</div>");
+
+            pos.append(demo1)
+
+            $('#change_div')
+                .on("click", function () {
+                    $.get("/api/demo/hello?name=you", function (data, status) {
+                        $('#replace_me').text(data.message);
+                        $('#change_div').removeClass("alert-info")
+                            .addClass("alert-success");
+                    });
+                });
+        },
+
+        init: function() {
+            this.demo($('#toolbar'))
+        }
+
+    }
+}(jQuery));
+
+
+
+jQuery(document).ready(function() {
+
+    DemoWidget.init();
+
+});
