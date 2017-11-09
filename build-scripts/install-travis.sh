566553a303aa46c3cdf202fd2c28cac0cb7b8c2f
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2017-11-09 14:35:31 +0100

    Fix typo

--- build-scripts/install-travis.sh
+++ build-scripts/install-travis.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/OpenSITMUN/sitmun-pof-plugin-core.git $PLUGIN_DIR/sitmun-pof-plugin-core
 cd $PLUGIN_DIR/sitmun-pof-plugin-core
-gradlew clean install
+gradle clean install
