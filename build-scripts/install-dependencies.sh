d9536e92b0d52112e3aba816e8480b95aa21b2bc
rbejar <rbejar@unizar.es>
2018-05-07 16:27:18 +0200

    Cleanup after fixing travis build process

--- build-scripts/install-dependencies.sh
+++ build-scripts/install-dependencies.sh
@@ -11,7 +11,7 @@ if ./gradlew cloneDependencyRepos; then
     echo
     echo "Repos cloned ..."
     echo
-    for D in $PLUGIN_DIR/*/; do echo "Clean install: "; echo ${D}; cd ${D}; ./gradlew clean install --stacktrace; done
+    for D in $PLUGIN_DIR/*/; do cd ${D}; ./gradlew clean install; done
 else
     echo
     echo "Install dependencies script FAILED"
