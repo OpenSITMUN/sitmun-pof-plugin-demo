0acc3f7ec5bcd74f430ed68c3079bfc79798cc30
Juan López de Larrínzar <jlarrinzar@gmail.com>
2019-01-11 11:15:48 +0100

    Win script fix

--- scripts-win/build-sitmun-plugin-demo.cmd
+++ scripts-win/build-sitmun-plugin-demo.cmd
@@ -1,6 +1,14 @@
 @ECHO OFF
-ng build sitmun-plugin-demo
-IF EXIST '.\dist\sitmun-plugin-demo\geoadmin-module-lib' GOTO COPYRESOURCES
-md '.\dist\sitmun-plugin-demo\geoadmin-module-lib'
+IF EXIST .\dist GOTO CHECKPLUGINDIR
+md .\dist
+:CHECKPLUGINDIR
+IF EXIST .\dist\sitmun-plugin-demo\geoadmin-module-lib GOTO CHECKMODULEDIR
+md .\dist\sitmun-plugin-demo
+:CHECKMODULEDIR
+IF EXIST .\dist\sitmun-plugin-demo\geoadmin-module-lib GOTO COPYRESOURCES
+echo 'Create directory'
+md .\dist\sitmun-plugin-demo\geoadmin-module-lib
 :COPYRESOURCES
-xcopy '.\src\main\angular-library\projects\sitmun-plugin-demo\src\lib\geoadmin-module\geoadmin-module-lib' '.\dist\sitmun-plugin-demo\'
+echo 'Copy resources'
+xcopy .\src\main\angular-library\projects\sitmun-plugin-demo\src\lib\geoadmin-module\geoadmin-module-lib .\dist\sitmun-plugin-demo\geoadmin-module-lib /E /H /Y
+ng build sitmun-plugin-demo
